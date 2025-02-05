bccca4762d958f92ffe4ece957d3f5aba995fa1d
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-02-05 15:23:49 +0100

    WIP: Solana wallet-standard

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -1467,7 +1467,8 @@ export const getAnySnapUpdateAvailable = createSelector(
 export const getHideSnapBranding = createDeepEqualSelector(
   [selectInstalledSnaps, selectSnapId],
   (installedSnaps, snapId) => {
-    return installedSnaps[snapId]?.hideSnapBranding;
+    return true;
+    // return installedSnaps[snapId]?.hideSnapBranding;
   },
 );
 
